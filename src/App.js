26aed9d77b53d91ebe97f62c9efde50b7e52d6af
cankien <cankien93@gmail.com>
2020-05-19 22:43:54 +0900

    create hat route

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
 import React from 'react';
 import './App.css';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route} from 'react-router-dom';
 import Homepage from './pages/homepage/HomePage';
 
 const HatsPage = props =>{
   console.log(props)
   return(
   <div>
-    <button onClick={() => props.history.push('/topics')}>Topics </button>
+    {/* <button onClick={() => props.history.push('/topics')}>Topics </button> */}
     <h1>HATS PAGE </h1>
   </div>)
 }
 
-const TopicsList = props =>{
-  console.log(props)
-  return(
-  <div>
-    <h1>TOPIC LIST PAGE</h1>
-    <Link to={`${props.match.url}/13`}>TO TOPIC 13</Link>
-    <Link to={`${props.match.url}/17`}>TO TOPIC 17</Link>
-    <Link to={`${props.match.url}/21`}>TO TOPIC 21</Link>
-  </div>)
-}
+// const TopicsList = props =>{
+//   console.log(props)
+//   return(
+//   <div>
+//     <h1>TOPIC LIST PAGE</h1>
+//     <Link to={`${props.match.url}/13`}>TO TOPIC 13</Link>
+//     <Link to={`${props.match.url}/17`}>TO TOPIC 17</Link>
+//     <Link to={`${props.match.url}/21`}>TO TOPIC 21</Link>
+//   </div>)
+// }
 
-const TopicDetail = props =>{
-  console.log(props)
-  return(
-  <div>
-    <h1>TOPIC DETAIL PAGE: {props.match.params.topicId}</h1>
-  </div>)
-}
+// const TopicDetail = props =>{
+//   console.log(props)
+//   return(
+//   <div>
+//     <h1>TOPIC DETAIL PAGE: {props.match.params.topicId}</h1>
+//   </div>)
+// }
 
 function App() {
   return (
@@ -37,8 +37,8 @@ function App() {
     <Switch>
       <Route exact path='/' component={Homepage} />
       <Route path='/hats' component={HatsPage} />
-      <Route exact path='/topics' component={TopicsList} />
-      <Route path='/topics/:topicId' component={TopicDetail} />
+      {/* <Route exact path='/topics' component={TopicsList} />
+      <Route path='/topics/:topicId' component={TopicDetail} /> */}
     </Switch>
     </div>
   );
--- src/components/menu-item/menu-item.js
+++ src/components/menu-item/menu-item.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './menu-item.scss'
 
-const Item = ({item, url, size}) => {
+const Item = ({item, imageUrl, size, history, linkUrl, match}) => {
     return(
-        <div className={`${size} menu-item`}>   
-            <div style={{backgroundImage:`url(${url})`}} className='background-image'/>
+        <div className={`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>   
+            <div style={{backgroundImage:`url(${imageUrl})`}} className='background-image'/>
             <div className='content'>
                 <h1 className='title'>{item}</h1>
                 <div className='subtitle'>SHOP NOW</div>
@@ -13,4 +14,4 @@ const Item = ({item, url, size}) => {
     )
 }
 
-export default Item;
+export default withRouter(Item);

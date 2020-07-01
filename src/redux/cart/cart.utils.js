f7b7d36397626982da4143536b81a8ff0d8747ce
cankien <cankien93@gmail.com>
2020-07-01 22:44:49 +0900

    adding arrow to increasing or decrement quantity of products in the cart, adding function successfull

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -13,3 +13,20 @@ export const addItemToCart = (cartItems, cartItemToAdd) =>{
 
     return [...cartItems, {...cartItemToAdd, quantity: 1}]
 };
+
+export const removeItemFromCart = (cartItems, ItemToRemove) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === ItemToRemove.id
+    )
+
+    if(existingCartItem.quantity === 1){
+        return cartItems.filter(item => item.quantity !==1)
+    }
+    else{
+        return cartItems.map(cartItem =>
+            cartItem.id === ItemToRemove.id 
+                ? {...cartItem, quantity: cartItem.quantity-1}
+                : cartItems
+        )
+    }
+}

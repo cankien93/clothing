2526f716ba7d66a20a15d5481ef6829b3f13a756
cankien <cankien93@gmail.com>
2020-07-30 21:31:42 +0900

    remove remain console.log Obj

--- src/components/menu-item/menu-item.js
+++ src/components/menu-item/menu-item.js
@@ -3,7 +3,6 @@ import { withRouter } from 'react-router-dom';
 import './menu-item.scss'
 
 const Item = ({item, imageUrl, size, history, linkUrl, match}) => {
-    console.log(history)
     return(
         <div className={`${size} menu-item`} 
             onClick={()=>{
